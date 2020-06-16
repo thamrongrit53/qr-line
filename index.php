d82dbfc9241914b8bb25ffb5647e9ad10eaddcb5
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-06-17 00:26:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>My LIFF App</title>
+  <title>MY APP</title> 
+  <style>
+    #pictureUrl { display: block; margin: 0 auto }
+  </style>
 </head>
 <body>
   <p id="scanCode"></p>
@@ -19,6 +22,31 @@
       });
     }
     liff.init({ liffId: "1654351482-62KJ4VlG" }, () => {}, err => console.error(err.code, error.message));
+  </script>
+  
+  <img id="pictureUrl" width="25%">
+  <p id="userId"></p>
+  <p id="displayName"></p>
+  <p id="statusMessage"></p>
+  <p id="getDecodedIDToken"></p>
+  <script src="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
+  <script>
+    function runApp() {
+      liff.getProfile().then(profile => {
+        document.getElementById("pictureUrl").src = profile.pictureUrl;
+        document.getElementById("userId").innerHTML = '<b>UserId:</b> ' + profile.userId;
+        document.getElementById("displayName").innerHTML = '<b>DisplayName:</b> ' + profile.displayName;
+        document.getElementById("statusMessage").innerHTML = '<b>StatusMessage:</b> ' + profile.statusMessage;
+        document.getElementById("getDecodedIDToken").innerHTML = '<b>Email:</b> ' + liff.getDecodedIDToken().email;
+      }).catch(err => console.error(err));
+    }
+    liff.init({ liffId: "YOUT-LIFF-ID" }, () => {
+      if (liff.isLoggedIn()) {
+        runApp()
+      } else {
+        liff.login();
+      }
+    }, err => console.error(err.code, error.message));
   </script>
 </body>
 </html>
