853a21fd229c2da9b472e8e57167ba046e077782
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-06-18 00:50:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,36 +5,11 @@
     <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js" ></script>	
   </head>
   <body>
+    <h2>Sbac Care</h2>
     <video id="preview" width="300"></video>
 
-  <h2>Sbac Care</h2>
-  <img id="pictureUrl" width="25%">
-  <p id="userId"></p>
-  <p id="displayName"></p>
-  <p id="statusMessage"></p>
-  <p id="getDecodedIDToken"></p>
-
-   <script src="https://static.line-scdn.net/liff/edge/2.1/liff.js"></script>
-</div>
- 
-  <script>
-    function runApp() {
-      liff.getProfile().then(profile => {
-        document.getElementById("pictureUrl").src = profile.pictureUrl;
-        document.getElementById("userId").innerHTML = '<b>UserId:</b> ' + profile.userId;
-        document.getElementById("displayName").innerHTML = '<b>DisplayName:</b> ' + profile.displayName;
-        document.getElementById("statusMessage").innerHTML = '<b>StatusMessage:</b> ' + profile.statusMessage;
-        document.getElementById("getDecodedIDToken").innerHTML = '<b>Email:</b> ' + liff.getDecodedIDToken().email;
-      }).catch(err => console.error(err));
-    }
-    liff.init({ liffId: "1654351482-62KJ4VlG" }, () => {
-      if (liff.isLoggedIn()) {
-        runApp()
-      } else {
-        liff.login();
-      }
-    }, err => console.error(err.code, error.message));
-  </script>
+  
+  
     <script>
         let scanner = new Instascan.Scanner(
             {
