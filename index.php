8b03341817ab0ffe40f7e5f73f9e92a203209cea
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-06-17 19:48:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,27 +3,32 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Sbac Care</title> 
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
    <script src="https://static.line-scdn.net/liff/edge/2.1/liff.js"></script>
   <style>
     #pictureUrl { display: block; margin: 0 auto }
   </style>
 </head>
 <body>
-  
+  <div class="container">
+  <h2>Sbac Care</h2>
+  <div class="card" style="width:400px">
+    <img class="card-img-top"id="pictureUrl" alt="Card image" style="width:100%">
+    <div class="card-body">
+      <h4 class="card-title" id="displayName"></h4>
+      <p class="card-text" id="statusMessage"></p>
+    </div>
+  </div>
+<br>
+<p id="scanCode"></p>
+
+<button class="btn btn-primary" id="btnScanCode" onclick="scanCode();">Scan Code</button>
+</div>
  
-
-  <img id="pictureUrl" width="25%">
-  <p id="userId"></p>
-  <p id="displayName"></p>
-  <p id="statusMessage"></p>
-  <p id="getDecodedIDToken"></p>
-
-    <p id="scanCode"></p>
-  <p>
-    <button id="btnScanCode" onclick="scanCode();">Scan Code</button>
-  </p>
-
-
   <script>
     function runApp() {
       liff.getProfile().then(profile => {
@@ -45,8 +50,12 @@
      function scanCode() {
       liff.scanCode().then(result => {
         const stringifiedResult = JSON.stringify(result);
-        alert(stringifiedResult);
-        document.getElementById("scanCode").textContent = stringifiedResult;
+        // alert(stringifiedResult);
+        // document.getElementById("scanCode").textContent = stringifiedResult;
+        if (stringifiedResult=="123") {
+            location.replace("https://www.w3schools.com")
+        }
+
       });
     }
     liff.init({ liffId: "1654351482-62KJ4VlG" }, () => {}, err => console.error(err.code, error.message));
