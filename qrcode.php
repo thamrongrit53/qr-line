3f888b79eeedbf0108387fb96e5966b32b0e67a2
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-06-18 00:40:52 +0700

    Update qrcode.php

--- qrcode.php
+++ qrcode.php
@@ -13,15 +13,15 @@
             }
         );
         scanner.addListener('scan', function(content) {
-            alert('Escaneou o conteudo: ' + content);
+            alert('value: ' + content);
             window.open(content, "_blank");
         });
         Instascan.Camera.getCameras().then(cameras => 
         {
             if(cameras.length > 0){
-                scanner.start(cameras[0]);
+                scanner.start(cameras[1]);
             } else {
-                console.error("Não existe câmera no dispositivo!");
+                console.error("no");
             }
         });
     </script>
