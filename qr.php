aac4298de8214e50435ce886a5f9713c5347599d
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-07-21 09:45:14 +0700

    Add files via upload

--- qr.php
+++ qr.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Instascan</title>
+    <title>SBAC</title>
     <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js" ></script>	
-  </head>
-  <body>
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css2?family=K2D:wght@700&display=swap"rel="stylesheet">
+  <style type="text/css">
+   footer {
+   
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   color: white;
+   text-align: center;
+}
+a{
+    color: white;
+}
+h1,p{
+  color: white;
+  }
+  </style>
+
+</head>
+<body style=" font-family: 'K2D', sans-serif;">
     <video id="preview"></video>
+    <div class="container">
+  <div class="row" style="margin-top:10px;"> 
+   <div class="col-md-12">
+    <div id="showData"></div>
+   </div>
+ </div>
+</div>
     <script>
         let scanner = new Instascan.Scanner(
             {
@@ -21,10 +53,26 @@
             if(cameras.length > 0){
                 scanner.start(cameras[0]);
             } else {
-                console.error("Não existe câmera no dispositivo!");
+                console.error("exit camera");
+
             }
         });
     </script>
-
+<script type="text/javascript">
+$(function(){
+    setInterval(function(){ // เขียนฟังก์ชัน javascript ให้ทำงานทุก ๆ 30 วินาที
+        // 1 วินาที่ เท่า 1000
+        // คำสั่งที่ต้องการให้ทำงาน ทุก ๆ 3 วินาที
+        var getData=$.ajax({ // ใช้ ajax ด้วย jQuery ดึงข้อมูลจากฐานข้อมูล
+                url:"http://sbac.online/school_attendance/gdata.php",
+                data:"rev=1",
+                async:false,
+                success:function(getData){
+                    $("div#showData").html(getData); // ส่วนที่ 3 นำข้อมูลมาแสดง
+                }
+        }).responseText;
+    },1000);    
+});
+</script>
  </body>
 </html>
