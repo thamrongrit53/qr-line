bd17c8223fea24813d0bfd69988d3016672fb095
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-07-21 09:51:06 +0700

    Add files via upload

--- qr.php
+++ qr.php
@@ -1,6 +1,4 @@
 <?php 
-
-header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
