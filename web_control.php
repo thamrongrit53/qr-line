6b90a3844ee99ca4e78966bc03b209ae9adf25ef
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-08-28 02:22:45 +0700

    Update web_control.php

--- web_control.php
+++ web_control.php
@@ -1,4 +1,4 @@
 <?php
-$arr = array('id'=>1,'name_ch1' => 1, 'ch1' => 'off','name_ch2'=>2,'ch2'=>'on');
+$arr = array('id'=>1,'name_ch1' => 1, 'ch1' => 'on','name_ch2'=>2,'ch2'=>'on');
 echo json_encode($arr);
 ?>
