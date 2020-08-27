b8e88a6d702f32f265f04dc2b71ab0ea81acffe2
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-08-28 02:36:53 +0700

    Add files via upload

---
+++ wcp.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Web Controller LED</h2>
+
+<form action="web_control.php">
+  <label for="fname">name_ch1:</label><br>
+  <input type="text"  name="name_ch1"><br>
+  <label for="lname">ch1:</label><br>
+  <input type="text" name="ch1" ><br><br>
+  <label for="fname">name_ch2:</label><br>
+  <input type="text"  name="name_ch2"><br>
+  <label for="lname">ch2:</label><br>
+  <input type="text" name="ch2" ><br><br>
+  <input type="submit" value="Submit">
+</form> 
+</body>
+</html>
--- web_control.php
+++ web_control.php
@@ -1,4 +1,10 @@
 <?php
-$arr = array('id'=>1,'name_ch1' => 1, 'ch1' => 'on','name_ch2'=>2,'ch2'=>'on');
+$name_ch1=$_POST["name_ch1"];
+$ch1=$_POST["ch1"];
+$name_ch2=$_POST["name_ch2"];
+$ch2=$_POST["ch2"];
+
+
+$arr = array('id'=>1,'name_ch1' =>$name_ch1, 'ch1' => $ch1,'name_ch2'=>$name_ch2,'ch2'=>$ch2);
 echo json_encode($arr);
 ?>
