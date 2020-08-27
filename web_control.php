f9ba27bf3a10d769ddcbaf9b49efc53b4164e98c
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-08-28 02:47:48 +0700

    Update web_control.php

--- web_control.php
+++ web_control.php
@@ -1,10 +1,6 @@
 <?php
-$name_ch1=$_POST["name_ch1"];
-$ch1=$_POST["ch1"];
-$name_ch2=$_POST["name_ch2"];
-$ch2=$_POST["ch2"];
 
+$arr = array('id'=>1,'name_ch' => 1, 'ch' => 'on');
 
-$arr = array('id'=>1,'name_ch1' =>$name_ch1, 'ch1' => $ch1,'name_ch2'=>$name_ch2,'ch2'=>$ch2);
 echo json_encode($arr);
 ?>
