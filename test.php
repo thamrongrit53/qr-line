ac34e0617d5d059d8f871ddf93cbd9ba1eae3446
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-08-30 01:21:44 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php 
 if(!empty($_POST['status']) && !empty($_POST['station']))
     {
-    	$status = $_POST['status'];
+    	 $status = $_POST['status'];
     	$station = $_POST['station'];
-  
- 	}
- 
-     echo $status;
+   echo $status;
      echo $station; 
 	
+ 	}
+ 
+    
 ?>
