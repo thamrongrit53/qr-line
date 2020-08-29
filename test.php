ab48fb474a36bd7e271f94ce42c0593ea437be95
thamrongrit53 <47946165+thamrongrit53@users.noreply.github.com>
2020-08-30 01:26:20 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,13 @@
 <?php 
 if(!empty($_POST['status']) && !empty($_POST['station']))
     {
-    	 $status = $_POST['status'];
-    	$station = $_POST['station'];
-   echo $status;
-     echo $station; 
-	
- 	}
- 
+		$myfile = fopen("log_data.txt", "w") or die("Unable to open file!");
+		$txt = $_POST['status']." ".$_POST['station'];
+		fwrite($myfile, $txt);
+		fclose($myfile);
+	}
+	$myfile = fopen("log_data.txt", "r") or die("Unable to open file!");
+	echo fgets($myfile);
+	fclose($myfile);
     
 ?>
